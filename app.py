a867299dfa12bb1f52d8c37b39f4032c2b11b7ef
amitdwi <amitdubey.108@gmail.com>
2024-03-20 18:49:00 +0530

    added register root

--- app.py
+++ app.py
@@ -1,42 +1,42 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-from json import JSONEncoder
-from bson import json_util
+#from json import JSONEncoder
+#from bson import json_util
 import urllib
 import certifi
+import bcrypt
 
 app = Flask(__name__)
 
-uri = "mongodb+srv://amitdwivedi:"+ urllib.parse.quote("Welcome@123") + "@cluster0.0zm9el6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://amitdwivedi:"+ urllib.parse.quote("12345") + "@cluster0.0zm9el6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
 db = client['my_db']
 collection = db['users']
 
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        return json_util.default(obj)
+user_schema = {
+    'email': str,
+    'password': str
+}
 
-app.json_encoder = CustomJSONEncoder
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
 
 @app.route('/')
-
 def home():
     return 'Welcome to Home page'
 
-@app.route('/users/<int:user_id>', methods=['GET'])
 
-def get_user(user_id):
-    user = collection.find_one({"id": user_id})
-    
-    return json_util.dumps(user), 200
-
-@app.route('/users', methods=['POST'])
-
-def create_user():
-    data = request.get_json()
-    collection.insert_one({"id": data['id'], "name": data['name'], "age": data['age']})
-    return jsonify({"message": "User added successfully"}), 201
+@app.route('/register', methods=['POST'])
+def user_register():
+    user_data = request.json
+    new_user = {key: user_data[key] for key in user_schema.keys()}
+    new_user['password'] = hash_password(new_user['password'])
+    result = collection.insert_one(new_user)
+    return jsonify({'message': 'User registered successfully'}), 201
 
 
 if __name__ == "__main__":
